33cecea19375c64031ee999890f3e9e2929e53e6
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-02-05 16:17:33 -0600

    trying to troubleshoot

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
-// app.get('/*', (req, res) => {
+// app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-//   }); 
+//   });
   
 
 module.exports = app;  
