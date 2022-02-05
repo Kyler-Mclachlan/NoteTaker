a1f59d54c5b7c9f78ceb87bbf35539894e35c894
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-02-05 16:16:00 -0600

    starting over since AskBCS broke parts of my code

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { notes } = require('./Develop/db/db.json');
-const res = require('express/lib/response');
-const apiRoutes = require('./Develop/routes/apiRoutes');
-const htmlRoutes = require('./Develop/routes/htmlRoutes')
-
-// insert routes here
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,14 +9,31 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
-app.use('/notes', apiRoutes);
-app.use('/', htmlRoutes);
+
+// api routes
+
 app.post('/notes', (req, res) => {
     // req.body is where our incoming content will be
     console.log('post recieved!');
     console.log(req.body);
     res.json(req.body);
 });
+
+// html routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+//   }); 
+  
+
+module.exports = app;  
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
